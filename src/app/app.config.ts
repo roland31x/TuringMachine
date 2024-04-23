3a1bcc949fa27d5a6ef03d87bce0bb54d968821e
roland31x <roland31x@gmail.com>
2024-04-24 02:21:21 +0300

    add ssr and new live version

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,11 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideAnimations()
+    provideAnimations(), provideClientHydration()
   ]
 };
